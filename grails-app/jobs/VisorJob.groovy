2584656ebf6271b262d176d05050f457b83a0c87
guido8a <guido8a@gmail.com>
2020-01-14 08:12:12 -0500

    procesa datos por separado y datos hoy...

--- grails-app/jobs/VisorJob.groovy
+++ grails-app/jobs/VisorJob.groovy
@@ -12,18 +12,18 @@ class VisorJob {
 
     static triggers = {
         simple startDelay: 1000*60*1, repeatInterval: 1000*60*60*8  /* cada 10 minutos */
-//        simple startDelay: 1000*20, repeatInterval: 1000*60*60*50  /* cada 10 minutos */
+//        simple startDelay: 1000 * 3, repeatInterval: 1000 * 60 * 60 * 50  /* cada 10 minutos */
     }
 
 
     /**
-    * cada intervalo ejecutar:
-    * 1. verifica si hay archivos nuevos y los copia a data y dataIUV  --> mueveArch()
-    * 2. ejecuta cargar datos              --> leeCSV(tipo)
-    * 3. ejecuta cargar IUV                --> cargaIUV()
-    * 4. ejecuta calcular datos derivados  --> calcular()
-    * 5. ejecuta calcular dirección        --> calcularDir()
-    **/
+     * cada intervalo ejecutar:
+     * 1. verifica si hay archivos nuevos y los copia a data y dataIUV  --> mueveArch()
+     * 2. ejecuta cargar datos              --> leeCSV(tipo)
+     * 3. ejecuta cargar IUV                --> cargaIUV()
+     * 4. ejecuta calcular datos derivados  --> calcular()
+     * 5. ejecuta calcular dirección        --> calcularDir()
+     **/
 
     void execute() {
         def cont = 1
@@ -34,12 +34,13 @@ class VisorJob {
         cargaArchivo('prod')
         calcular()
         calcularDir()
+        activar()
+        calcularHoy()
+        calcularDirHoy()
 
         println "Fin procesos automáticos: ${new Date()}"
 //        lecturasService.leeCSV('prueba')  /* no se usa */
 //        lecturasService.leeCSV('prod')  /* no se usa */
-
-
     }
 
 
@@ -62,7 +63,7 @@ class VisorJob {
         def prob_arch = "PROBLEMAS ${new Date()}"
         def problemas = 0
 
-        if(grails.util.Environment.getCurrent().name == 'development') {
+        if (grails.util.Environment.getCurrent().name == 'development') {
 //            directorio = '/home/guido/proyectos/visor/dataIUV/2018'
             directorio = '/home/guido/proyectos/visor/data'
         } else {
@@ -101,7 +102,7 @@ class VisorJob {
                         if (cuenta < procesa) {
 //                        println "${line}"
 
-                            if(line.toString() =~ /\d.[a-z]./) {
+                            if (line.toString() =~ /\d.[a-z]./) {
                                 line = line.replace(",", ".")
                             }
 
@@ -114,16 +115,16 @@ class VisorJob {
                                 estc = lecturasService.datosEstaciones(rgst)
 //                                if(estc && cuenta == 0) cuenta = 1
 //                                println "estaciones: $estc"
-                            } else if (cuenta ==1) {
+                            } else if (cuenta == 1) {
 //                                println ">>cuenta: $cuenta, registro: $rgst"
-                                if(rgst[1].toString().contains('Ed')) {
+                                if (rgst[1].toString().contains('Ed')) {
                                     rgst = rgst*.replaceAll(('EdPAR'), ('PAR'))
                                 }
                                 mg = rgst[1..-1]
 //                                println "buisca magn: $mg"
                                 magn = lecturasService.buscaMagnIUV(mg)
 //                                println "-----> mag: ${magn[0]} ---> $rgst"
-                                if(magn[0] == null) {
+                                if (magn[0] == null) {
                                     prob_magn += "\n${mg}"
                                     lecturasService.archivoProblema(arch, mg)
                                     problemas++
@@ -133,7 +134,7 @@ class VisorJob {
                             } else if (rgst[0] && rgst[0] != 'FECHA' && rgst[0].toString().toLowerCase() != 'date') {
 //                            println "\n cuenta: $cuenta, fecha: ${rgst[0]}"
 //                                fcha = new Date().parse('yyyy-MM-dd HH:mm:ss', rgst[0])
-                                if(rgst[0].toString() =~ /\d.[a-z]./) {
+                                if (rgst[0].toString() =~ /\d.[a-z]./) {
                                     fcha = new SimpleDateFormat("dd-MMM-yyyy HH:mm").parse(rgst[0])
                                 } else {
                                     fcha = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").parse(rgst[0])
@@ -142,7 +143,7 @@ class VisorJob {
                                 rgst[0] = fcha
 //                            println "---> Registro: $rgst"
 
-                                if(magn[0] && estc[0]) {
+                                if (magn[0] && estc[0]) {
                                     inserta = lecturasService.cargarLectIUV(rgst, magn, estc)
                                     cont += inserta.insertados
                                     repetidos += inserta.repetidos
@@ -153,9 +154,9 @@ class VisorJob {
                             }
 
 //                            if(rgst.size() > 0 && rgst[1]) cuenta++  /* se cuentan sólo si hay valores */
-                            if(cuenta > 3 && rgst[1] != 0) {  /* se cuentan sólo si hay valores */
+                            if (cuenta > 3 && rgst[1] != 0) {  /* se cuentan sólo si hay valores */
                                 cuenta++
-                            }  else {
+                            } else {
                                 cuenta++
                             }
 
@@ -222,7 +223,7 @@ class VisorJob {
                             "where magnitude_id = ${mg.id} and opoint_id = ${es.id} and avg1m is not null"
 //                    print "mg--> ${mg.id}: $sql1"
                     cn.eachRow(sql1.toString()) { d ->
-                        if(d.fcin && d.fcfn) {
+                        if (d.fcin && d.fcfn) {
 //                            println "fcin: ${d.fcin}, fcfn: ${d.fcfn}"
 //                            desde = new Date().parse("yyyy-MM-dd", "${d.fcfn}")
                             desde = new SimpleDateFormat("yyyy-MM-dd").parse("${d.fcin}")
@@ -238,7 +239,7 @@ class VisorJob {
                     fcha = desde
 //                    fchaFin = new Date().parse("dd-MM-yyyy", "31-12-${fcha.format('yyyy')}")
 //                    fchaFin = new SimpleDateFormat("dd-MM-yyyy").parse("31-12-${fcha.format('yyyy')}")
-                    fchaFin = new SimpleDateFormat("dd-MM-yyyy").parse("31-12-${fcha.getYear()+1900}")
+                    fchaFin = new SimpleDateFormat("dd-MM-yyyy").parse("31-12-${fcha.getYear() + 1900}")
 //                    println "procesa desde ${desde} hasta: ${fchaFin}"
                     while (fcha < hasta) {
                         use(groovy.time.TimeCategory) {
@@ -362,7 +363,116 @@ class VisorJob {
 //        return "Porcesado: ${salidaTotal}"
     }
 
+    def activar() {
+        println "activar -->"
+        def cn = dbConnectionService.getConnection()
+        def sql = ""
+        def magn = ""
+        def estc = ""
+        def nmro = 0
 
+        sql = "select distinct magnitude_id id from survey.data"
+        cn.eachRow(sql.toString()) { m ->
+            magn += magn ? ", ${m.id}" : m.id
+        }
+        println "magnitudes $magn"
+
+        sql = "select distinct opoint_id id from survey.data"
+        cn.eachRow(sql.toString()) { e ->
+            estc += estc ? ", ${e.id}" : e.id
+        }
+        println "estaciones $estc"
+
+        sql = "update survey.magnitude set active = 'S' where id in ($magn)"
+        println "sql --> $sql"
+        cn.execute(sql.toString())
+        nmro += cn.updateCount
+
+        sql = "update survey.magnitude set active = 'N' where id not in ($magn)"
+        println "sql --> $sql"
+        cn.execute(sql.toString())
+        nmro += cn.updateCount
+
+        sql = "update survey.opoint set active = 'S' where id in ($estc)"
+        println "sql --> $sql"
+        cn.execute(sql.toString())
+        nmro += cn.updateCount
+
+        sql = "update survey.opoint set active = 'N' where id not in ($estc)"
+        println "sql --> $sql"
+        cn.execute(sql.toString())
+        nmro += cn.updateCount
+
+        println "Porcesado: ... $nmro registros"
+    }
+
+
+    def calcularHoy() {
+        println "calculaHoy job -->"
+        def cn = dbConnectionService.getConnection()
+        def sql = ""
+        def magn = []
+        def estc = []
+        def salida = ""
+        def proceso = ['10 minutes', '1 hours', '8 hours', '24 hours', '72 hours']
+        def fchaFin = new Date()
+        def fcha = fchaFin - 90
+
+        sql = "select distinct id from survey.magnitude where active = 'S' and id != 82 order by 1"
+        magn = cn.rows(sql.toString())
+        proceso.each { prcs ->
+            magn.each { mg ->
+                print " procesa ${prcs}: magnitud: ${mg.id} "
+
+                sql = "select distinct id id from survey.opoint where active = 'S'"
+                estc = cn.rows(sql.toString())
+                estc.each { es ->
+                    sql = "select * from survey.promedios(${mg.id}, ${es.id}, '${prcs}', " +
+                            "'${fcha.format('yyyy-MM-dd')}', '${fchaFin.format('yyyy-MM-dd')}')"
+//                    println "sql--> $sql"
+                    cn.eachRow(sql.toString()) { dt ->
+                        salida = dt.promedios
+                    }
+                    print "..${es.id}"
+                }
+                println ""
+            }
+        }
+    }
+
+
+    def calcularDirHoy() {
+        println "calcula dir hoy job -->"
+        def cn = dbConnectionService.getConnection()
+        def sql = ""
+        def magn = []
+        def estc = []
+        def salida = ""
+        def proceso = ['10 minutes', '1 hours', '8 hours', '24 hours', '72 hours']
+        def fchaFin = new Date()
+        def fcha = fchaFin - 90
+
+        sql = "select distinct id from survey.magnitude where active = 'S' and id = 82"
+        magn = cn.rows(sql.toString())
+        proceso.each { prcs ->
+            magn.each { mg ->
+                print " procesa dirrección ${prcs}: magnitud: ${mg.id} "
+
+                sql = "select distinct id id from survey.opoint where active = 'S'"
+                estc = cn.rows(sql.toString())
+                estc.each { es ->
+                    sql = "select * from survey.promedios_dir(${es.id}, '${prcs}', " +
+                            "'${fcha.format('yyyy-MM-dd')}', '${fchaFin.format('yyyy-MM-dd')}')"
+//                    println "sql--> $sql"
+                    cn.eachRow(sql.toString()) { dt ->
+                        salida = dt.promedios_dir
+                    }
+                    print "..${es.id}"
+                }
+                println ""
+            }
+        }
+    }
 
 
 }
